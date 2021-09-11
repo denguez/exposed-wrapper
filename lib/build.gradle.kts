725c2e58a9bb889c59238b628f6caa34acffdafe
jc <jc@pop-os.localdomain>
2021-09-11 15:50:10 +0200

    fix dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
-    `java-library`
+    //`java-library`
 }
 
 repositories {
@@ -19,8 +19,6 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.0-jre")
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     // Use the Kotlin JUnit integration.
