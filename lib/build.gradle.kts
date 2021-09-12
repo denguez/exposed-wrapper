8d9a74c28d3b7f8928fbf21917a4827cba2cd726
jc <jc@pop-os.localdomain>
2021-09-12 12:01:05 +0200

    fix build script

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.31"
+    kotlin("jvm") version "1.5.0"
     `maven-publish`
-    `java-library`
 }
 
 repositories {
@@ -9,20 +8,18 @@ repositories {
 }
 
 dependencies {
-    // Database
+    // Postgres
 	runtimeOnly("org.postgresql:postgresql:42.2.2")
-    implementation("org.jetbrains.exposed:exposed-core:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.31.1")
 
-    // Align versions of all Kotlin components
+    // Exposed
+    api("org.jetbrains.exposed:exposed-core:0.31.1")
+    api("org.jetbrains.exposed:exposed-dao:0.31.1")
+    api("org.jetbrains.exposed:exposed-jdbc:0.31.1")
+    api("org.jetbrains.exposed:exposed-java-time:0.31.1")
+
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
@@ -31,12 +28,8 @@ publishing {
         val mavenJava by creating(MavenPublication::class) {
             groupId = "com.github.denguez"
             artifactId = "reifydb"
-            version = "1.1"
+            version = "1.2"
             from(components["java"])
         }
     }
-}
-
-java {
-    withSourcesJar()
 }
