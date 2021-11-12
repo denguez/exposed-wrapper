3a957c4cfcbfe3ecc3d64a560e12c9cc169e030f
jc <jc@pop-os.localdomain>
2021-11-12 19:16:34 +0100

    variable driver

--- lib/src/main/kotlin/reifydb/Database.kt
+++ lib/src/main/kotlin/reifydb/Database.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class DatabaseConfig() {
-    val driver = "org.postgresql.Driver"
+    var driver = "org.postgresql.Driver"
     var url = ""
     var username = "root"
     var password = ""
