506e315d4665743afd8d3e28108c3f1ec8313b70
jc <jc@pop-os.localdomain>
2021-11-12 22:29:22 +0100

    refactoooor

--- lib/src/main/kotlin/reifydb/DAO.kt
+++ lib/src/main/kotlin/reifydb/DAO.kt
@@ -44,11 +44,23 @@ abstract class IDDAO<I : Comparable<I>, T : IDEntity<I>>(table: IDTable<I>) :
 }
 
 abstract class IntIDDAO<T : IntIDEntity>(table: IntIDTable) : IntEntityClass<T>(table) {
-    suspend open fun readById(id: Int): T? = transaction { findById(id) }
+    suspend open fun create(block: T.() -> Unit): T = transaction { new(block) }
 
-    suspend open fun update(id: Int, block: T.() -> Unit) = transaction {
-        findById(id)?.apply(block)
+    suspend open fun read(where: WhereExpression = { TRUE }) = transaction { find(where).toList() }
+    suspend open fun readOne(where: WhereExpression): T? = transaction { find(where).firstOrNull() }
+
+    suspend open fun update(entity: T, block: T.() -> Unit) = transaction { entity.apply(block) }
+    suspend open fun update(where: WhereExpression, block: T.() -> Unit) = transaction {
+        find(where).forEach { it.apply(block) }
     }
 
-    suspend open fun delete(id: Int) = transaction { findById(id)?.delete() }
+    suspend open fun delete(entity: T) = transaction { entity.delete() }
+    suspend open fun delete(where: WhereExpression) = transaction {
+        find(where).forEach { it.delete() }
+    }
+
+    suspend open fun <R : Comparable<R>> readSorted(
+            selector: (T) -> R?,
+            where: WhereExpression = { TRUE }
+    ) = transaction { find(where).sortedBy(selector) }.toList()
 }
--- lib/src/main/kotlin/reifydb/Table.kt
+++ lib/src/main/kotlin/reifydb/Table.kt
@@ -1,7 +1,7 @@
 package reifydb
 
 import org.jetbrains.exposed.dao.id.IdTable
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.dao.id.IntIdTable as SQLIntTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table as SQLTable
 import org.jetbrains.exposed.sql.`java-time`.date as sqlDate
@@ -13,14 +13,20 @@ abstract class Table(name: String = "") : SQLTable(name) {
     fun date(name: String) = sqlDate(name)
 }
 
-abstract class IntIDTable(name: String = "") : IntIdTable(name)
-
 abstract class IDTable<ID : Comparable<ID>>(name: String = "") : IdTable<ID>(name) {
-    fun <T> primaryKey(column: Column<T>) = PrimaryKey(column)
     fun string(name: String) = varchar(name, 255)
     fun date(name: String) = sqlDate(name)
 
     fun join(other: IDTable<ID>, where: WhereExpression) = run {
+        innerJoin(other).slice(columns).select(where).withDistinct()
+    }
+}
+
+abstract class IntIDTable(name: String = "") : SQLIntTable(name) {
+    fun string(name: String) = varchar(name, 255)
+    fun date(name: String) = sqlDate(name)
+
+    fun join(other: IntIDTable, where: WhereExpression) = run {
         innerJoin(other).slice(columns).select(where).withDistinct()
     }
 }
