f38a1bf8f1033e0746960eae8e7427fdc6b6793b
jc <jc@pop-os.localdomain>
2021-12-06 18:27:14 +0100

    add optional db parameter

--- lib/src/main/kotlin/reifydb/functions.kt
+++ lib/src/main/kotlin/reifydb/functions.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Op
 import org.jetbrains.exposed.sql.SqlExpressionBuilder
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.statements.UpdateStatement
 import org.jetbrains.exposed.sql.transactions.transaction as sqlTransaction
@@ -14,5 +15,5 @@ typealias WhereExpression = SqlExpressionBuilder.() -> Op<Boolean>
 typealias CreateExpression = Table.(InsertStatement<Number>) -> Unit
 typealias UpdateExpression = Table.(UpdateStatement) -> Unit
 
-suspend fun <T> transaction(block: () -> T) =
-        withContext(Dispatchers.IO) { sqlTransaction { block() } }
+suspend fun <T> transaction(db: Database? = null, block: () -> T) =
+        withContext(Dispatchers.IO) { sqlTransaction(db) { block() } }
